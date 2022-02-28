cb1b6bb9df5d6bcc2bb101618a4d3a2cb14c21fd
王子成 <wangzicheng@300.cn>
2022-02-28 16:48:15 +0800

    面试题pdf

--- src/com/leecode/pratice/PraticeDemo.java
+++ src/com/leecode/pratice/PraticeDemo.java
@@ -1,7 +1,7 @@
 package com.leecode.pratice;
 
 import com.leecode.dto.ListNode;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,14 +15,52 @@ import java.util.Map;
  */
 public class PraticeDemo {
     @Test
-    public void testDemo(){
-        ListNode listNode=new ListNode(1);
-        ListNode listNode1=new ListNode(3);
+    public void testDemo() {
+        ListNode listNode = new ListNode(1);
+        ListNode listNode1 = new ListNode(3);
 
-        List<ListNode> listNodes=new ArrayList<>();
+        List<ListNode> listNodes = new ArrayList<>();
         listNodes.add(listNode);
         listNodes.add(listNode1);
-        Map<String,List<ListNode>> map=new HashMap<>();
-        map.put("data",listNodes);
+        Map<String, List<ListNode>> map = new HashMap<>();
+        map.put("data", listNodes);
     }
+
+    @Test
+    public void testN() {
+        Long start = System.currentTimeMillis();
+        int n = 100000;
+        long k = 0;
+        for (long i = 0; i < n; i++) {
+            for (long j = i; j < n; j++) {
+                k++;
+            }
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println(end - start + "ms");
+    }
+
+    @Test
+    public void testAddDigits() {
+        System.out.println(addDigits(123123123));
+    }
+
+    public int addDigits(int num) {
+        //1232323
+        int x = num % 10;
+        int y = (num - x) / 10;
+        if (x + y < 10) {
+            return x + y;
+        } else {
+            int temp = x + addDigits(y);
+            if (temp < 10) {
+                return temp;
+            } else {
+                return addDigits(temp);
+            }
+        }
+        //return (num - 1) % 9 + 1;
+    }
+
+
 }
--- src/com/leecode/string/Convert.java
+++ src/com/leecode/string/Convert.java
@@ -2,6 +2,7 @@ package com.leecode.string;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,8 +35,59 @@ public class Convert {
     }
 
     @Test
-    public void testCouvert(){
+    public void testCouvert() throws ClassNotFoundException {
         String s =convert("sfsdfaaefcaceffae",4);
         System.err.println(s);
+        Class<Convert> convertClass = Convert.class;
+        //Calculator实现类的构造器信息
+        Constructor<?>[] calculatorImplClazzConstructors = convertClass.getConstructors();
+        //Calculator实现类的方法信息
+        Method[] calculatorImplClazzMethods = convertClass.getMethods();
+        //打印
+        System.out.println("------实现类Class的构造器信息------");
+        printClassInfo(calculatorImplClazzConstructors);
+        System.out.println("------实现类Class的方法信息------");
+        printClassInfo(calculatorImplClazzMethods);
+    }
+
+    public static void printClassInfo(Executable[] targets){
+        for (Executable target : targets) {
+            // 构造器/方法名称
+            String name = target.getName();
+            StringBuilder sBuilder = new StringBuilder(name);
+            // 拼接左括号
+            sBuilder.append('(');
+            Class[] clazzParams = target.getParameterTypes();
+            // 拼接参数
+            for(Class clazzParam : clazzParams){
+                sBuilder.append(clazzParam.getName()).append(',');
+            }
+            //删除最后一个参数的逗号
+            if(clazzParams!=null && clazzParams.length != 0) {
+                sBuilder.deleteCharAt(sBuilder.length()-1);
+            }
+            //拼接右括号
+            sBuilder.append(')');
+            //打印 构造器/方法
+            System.out.println(sBuilder.toString());
+        }
+    }
+
+
+    private static Object getProxy(final Object target) throws Exception {
+        Object proxy = Proxy.newProxyInstance(
+                target.getClass().getClassLoader(),/*类加载器*/
+                target.getClass().getInterfaces(),/*让代理对象和目标对象实现相同接口*/
+                new InvocationHandler(){/*代理对象的方法最终都会被JVM导向它的invoke方法*/
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println(method.getName() + "方法开始执行...");
+                        Object result = method.invoke(target, args);
+                        System.out.println(result);
+                        System.out.println(method.getName() + "方法执行结束...");
+                        return result;
+                    }
+                }
+        );
+        return proxy;
     }
 }
