2d2d847deffbf15b5309dd570ec2b339c7a2e0e8
王子成 <wangzicheng@300.cn>
2022-03-28 13:59:34 +0800

    归并排序

--- src/com/leecode/code/Code07_MergeSort.java
+++ src/com/leecode/code/Code07_MergeSort.java
@@ -10,6 +10,47 @@ package com.leecode.code;
  */
 public class Code07_MergeSort {
 
+    public int[] sortArray(int[] nums) {
+        sortArr(nums,0,nums.length-1);
+        return nums;
+    }
+
+    public void sortArr(int[] nums, int l, int r) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        int mid = l + ((l - r) >> 2);
+        sortArr(nums, l, mid);
+        sortArr(nums, mid + 1, r);
+        mergeArr(nums, l, mid, r);
+    }
+
+    private void mergeArr(int[] nums, int l, int mid, int r) {
+        if (l == r) {
+            return;
+        }
+        int[] temp = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = mid + 1;
+        while (p1<=mid && p2 <=r){
+            temp[i++] = nums[p1]<nums[p2]?nums[p1++]:nums[p2++];
+        }
+
+        while (p1<=mid){
+            temp[i++] = nums[p1++];
+        }
+
+        while (p2<=r){
+            temp[i++] = nums[p2++];
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            nums[l+i] = temp[j];
+        }
+    }
+
+
     public void sort(int[] arr, int l, int r) {
         if (arr == null || arr.length < 2) {
             return;
---
+++ src/com/leecode/code/Code07_QuickSort.java
@@ -0,0 +1,12 @@
+package com.leecode.code;
+
+/**
+ * @projectName: AlgorithmStudy
+ * @package: com.leecode.code
+ * @author: wangzicheng
+ * @description: TODO
+ * @date: 2022/3/27 11:48
+ * @version: 1.0.0
+ */
+public class Code07_QuickSort {
+}
