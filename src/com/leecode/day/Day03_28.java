9ad71a2083f1fc81ca58819025a487a36349ba0f
王子成 <wangzicheng@300.cn>
2022-03-29 17:49:23 +0800

    693. 交替位二进制数

--- src/com/leecode/day/Day03_28.java
+++ src/com/leecode/day/Day03_28.java
@@ -1,5 +1,7 @@
 package com.leecode.day;
 
+import com.leecode.dto.Util;
+
 /**
  * @projectName: AlgorithmStudy
  * @package: com.leecode.day
@@ -47,6 +49,85 @@ public class Day03_28 {
         }
         int a = n ^ (n >> 1);
         int b = a & (a + 1);
+    }
+
+    public static void SelectionSort(int[] arr) {
+        if (arr == null || arr.length < 1) {
+            return;
+        }
+
+        int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            int minIndex = i;
+            for (int j = i; j < N; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            Util.swap(arr, i, minIndex);
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 1) {
+            return;
+        }
+        int N = arr.length;
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[j + 1] && j + 1 < N) {
+                    Util.swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 1) {
+            return;
+        }
+        int N = arr.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] < arr[j + 1]; j--) {
+                Util.swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (arr == null || start >= end) {
+            return;
+        }
+
+        int mid = start + (end - start) >> 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        mergeGroups(arr, start, mid, end);
+    }
+
+    private static void mergeGroups(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start;
+        int j = mid +1 ;
+        int t = 0;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
+                temp[t++] = arr[i++];
+            } else {
+                temp[t++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[t++] = arr[i++];
+        }
+
+        while (j <= end) {
+            temp[t++] = arr[j++];
+        }
+
+        for (int k = 0; k < t; k++) {
+            arr[start + k] = temp[k];
+        }
     }
 
 }
