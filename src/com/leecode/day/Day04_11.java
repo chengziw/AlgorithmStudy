697247018a3223f56b99624ee2e4b28a0c20f4be
王子成 <wangzicheng@300.cn>
2022-04-11 21:37:15 +0800

    链表的练习

--- src/com/leecode/day/Day04_11.java
+++ src/com/leecode/day/Day04_11.java
@@ -75,4 +75,59 @@ public class Day04_11 {
         }
         return len;
     }
+
+    /**
+     * create by: wangzicheng
+     * description: https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+     * create time: 2022/4/11 21:15
+     * version: v1.0.0
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode qNode = head;
+        ListNode sNode = dummy;
+        for (int i = 0; i < n; i++) {
+            qNode = qNode.next;
+        }
+
+        while (qNode != null) {
+            qNode = qNode.next;
+            sNode = sNode.next;
+        }
+        sNode.next = sNode.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    /**
+     * create by: wangzicheng
+     * description: https://leetcode-cn.com/problems/rotate-list/
+     * create time: 2022/4/11 21:17
+     * version: v1.0.0
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null) {
+            return head;
+        }
+        int n = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            iter = iter.next;
+            n++;
+        }
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+        iter.next = head;
+        while (add-- > 0) {
+            iter = iter.next;
+        }
+        ListNode ret = iter.next;
+        iter.next = null;
+        return ret;
+    }
+
 }
